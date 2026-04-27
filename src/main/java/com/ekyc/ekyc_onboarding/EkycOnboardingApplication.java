42f016d9b105425d289262619e8f95672c5273ec
duydo <3d1911.doduy@gmail.com>
2026-04-27 22:48:36 +0700

    create project

---
+++ src/main/java/com/ekyc/ekyc_onboarding/EkycOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.ekyc.ekyc_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkycOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkycOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekyc/ekyc_onboarding/EkycOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekyc.ekyc_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkycOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
